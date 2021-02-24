2576ae960939ec19998c2fa4cf6aa466ab4f702f
ufochuxian <chen853409335@gmail.com>
2021-02-24 17:10:47 +0800

    refactor module

--- app/src/main/java/com/eric/androidplugin/MainActivity.kt
+++ app/src/main/java/com/eric/androidplugin/MainActivity.kt
@@ -5,11 +5,10 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.eric.plugin.LoadUtil
 
 
 class MainActivity : AppCompatActivity() {
-
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- lib_plugin/src/main/java/com/eric/plugin/LoadUtil.kt
+++ lib_plugin/src/main/java/com/eric/plugin/LoadUtil.kt
@@ -1,4 +1,4 @@
-package com.eric.androidplugin
+package com.eric.plugin
 
 import android.content.Context
 import android.util.Log
